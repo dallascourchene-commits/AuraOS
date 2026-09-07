4662b3b357ecff54e38f5c5b1dc1269fd53e1f42
dallascourchene-commits <dallascourchene@gmail.com>
2026-09-07 07:16:05 -0500

    arena: add MC-O14R Omega8

---
+++ tools/project006/mc_o14_lattice8.py
@@ -0,0 +1,17 @@
+from itertools import product
+from hashlib import sha256
+import json
+HARD=('producer_exact','consumer_exact_independent','verifier_authorization_exact','semantic_generation_current','full_source_operation_identity_exact','journal_schema_and_owner_current','durable_admission_exact','authority_separation')
+def hard_decision(a):
+    if len(a)!=8 or any(x not in (0,1,2) for x in a): return 'HOLD_MALFORMED'
+    if 0 in a:return 'HOLD_HARD_INVALID'
+    if 1 in a:return 'HOLD_UNRESOLVED'
+    return 'BIND_EFFECT_ATTEMPT_D0'
+def run():
+    d={'states':0,'bind':0,'invalid_bind':0}
+    for a in product(range(3),repeat=8):
+        x=hard_decision(a); valid=all(v==2 for v in a); d['states']+=1; d['bind']+=int(x=='BIND_EFFECT_ATTEMPT_D0'); d['invalid_bind']+=int(x=='BIND_EFFECT_ATTEMPT_D0' and not valid)
+    p={'schema':'aura.mc_o14r.omega8.v2','hard_axes':HARD,**d,'effect_ready':0,'authority':'D0_NONPROMOTING'}; p['root']=sha256(json.dumps(p,sort_keys=True,separators=(',',':')).encode()).hexdigest()
+    if p['states']!=6561 or p['bind']!=1 or p['invalid_bind']: raise AssertionError(p)
+    return p
+if __name__=='__main__':print(json.dumps(run(),sort_keys=True,indent=2))
